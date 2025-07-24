f55fd062e404f41c683837604fcd93c9c1a67e61
jawednur <133543592+jawednur@users.noreply.github.com>
2025-07-23 23:45:06 -0700

    Update base paths in Vite and Webpack configurations to '/Sticky-Notes/'

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
-    publicPath: '/games/',
+    publicPath: '/Sticky-Notes/',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
